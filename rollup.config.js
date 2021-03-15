4772722f50b217e950e0d0381e407f3d43fe1742
Jang Seokhyeon <coolman555@me.com>
2021-03-15 10:54:53 +0900

    fix: setting

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,157 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
 import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
 import pkg from "./package.json";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      exclude: "**/__tests__/**",
-      clean: true,
-    }),
-    commonjs({
-      include: ["node_modules/**"],
-      namedExports: {
-        "node_modules/react/react.js": [
-          "Children",
-          "Component",
-          "PropTypes",
-          "createElement",
-        ],
-        "node_modules/react-dom/index.js": ["render"],
-      },
-    }),
-  ],
+const extensions = [".ts"];
+const noDeclarationFiles = { compilerOptions: { declaration: false } };
+
+const babelRuntimeVersion = pkg.dependencies["@babel/runtime"].replace(
+  /^[^0-9]*/,
+  ""
+);
+
+const makeExternalPredicate = (externalArr) => {
+  if (externalArr.length === 0) {
+    return () => false;
+  }
+  const pattern = new RegExp(`^(${externalArr.join("|")})($|/)`);
+  return (id) => pattern.test(id);
 };
+
+export default [
+  // CommonJS
+  {
+    input: "src/index.ts",
+    output: { file: "lib/index.js", format: "cjs", indent: false },
+    external: makeExternalPredicate([
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ]),
+    plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      typescript({ useTsconfigDeclarationDir: true }),
+      babel({
+        extensions,
+        plugins: [
+          ["@babel/plugin-transform-runtime", { version: babelRuntimeVersion }],
+        ],
+        babelHelpers: "runtime",
+      }),
+    ],
+  },
+
+  // ES
+  {
+    input: "src/index.ts",
+    output: { file: "es/index.js", format: "es", indent: false },
+    external: makeExternalPredicate([
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ]),
+    plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        plugins: [
+          [
+            "@babel/plugin-transform-runtime",
+            { version: babelRuntimeVersion, useESModules: true },
+          ],
+        ],
+        babelHelpers: "runtime",
+      }),
+    ],
+  },
+
+  // ES for Browsers
+  {
+    input: "src/index.ts",
+    output: { file: "es/index.mjs", format: "es", indent: false },
+    plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      }),
+      typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        exclude: "node_modules/**",
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false,
+        },
+      }),
+    ],
+  },
+
+  // UMD Development
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.js",
+      format: "umd",
+      name: "index",
+      indent: false,
+    },
+    plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        exclude: "node_modules/**",
+      }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("development"),
+      }),
+    ],
+  },
+
+  // UMD Production
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.min.js",
+      format: "umd",
+      name: "index",
+      indent: false,
+    },
+    plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        exclude: "node_modules/**",
+      }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false,
+        },
+      }),
+    ],
+  },
+];

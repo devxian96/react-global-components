8ca78c7b34991903c193e40d7ce8bed0c15ace5d
Seokhyeon Jang <coolman555@me.com>
2021-06-18 13:44:56 +0900

    fix: all bug about type

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@
 if (typeof require.context === 'undefined') {
   const fs = require('fs');
   const path = require('path');
-  require.context = (base: String = '.', scanSubDirectories: Boolean = false, regularExpression: RegExp = /\.js$/): Function => {
+  require.context = (base: string = '.', scanSubDirectories: Boolean = false, regularExpression: RegExp = /\.js$/): any => {
     const files: Object = {};
 
     function readDirectory(directory: string) {
-      fs.readdirSync(directory).forEach((file: String) => {
-        const fullPath: String = path.resolve(directory, file);
+      fs.readdirSync(directory).forEach((file: string) => {
+        const fullPath: string = path.resolve(directory, file);
 
         if (fs.statSync(fullPath).isDirectory()) {
           if (scanSubDirectories) readDirectory(fullPath);
@@ -23,20 +23,22 @@ if (typeof require.context === 'undefined') {
 
     readDirectory(path.resolve(__dirname, base));
 
-    function Module(file: String): NodeRequire {
+    function Module(file: string): any {
       return require(file);
     }
 
     Module.keys = (): String[] => Object.keys(files);
+
+    console.log(typeof Module)
 
     return Module;
   };
 }
 
 // Import Global
-function importGlobal(requireContext: __WebpackModuleApi.RequireContext): void {
+function importGlobal(requireContext: any): void {
   requireContext.keys().map((key: string) => {
-    window[key.replace(".js", "").replace(/^.*[\\\/]/, '')] = requireContext(
+    global.globalThis[key.replace(".js", "").replace(/^.*[\\\/]/, '')] = requireContext(
       key
     ).default;
   });
--- tests/publish.test.ts
+++ tests/publish.test.ts
@@ -1,33 +1,31 @@
+declare var lodash: any;
 
+// @ts-ignore
 import ReactGlobalComponents from "react-global-components";
 import ReactGlobalComponentsLocal from "../src/index";
 
+const $this = (<any>globalThis)
+
 describe('Publish Version Test', (): void => {
   test("lodash is undefined?", (): void => {
-    // @ts-expect-error: Typescript can not read global variable
-    window.lodash = undefined
-    // @ts-expect-error: Typescript can not read global variable
-    expect(lodash === undefined).toBeTruthy();
+    $this.lodash = undefined
+    expect($this.lodash === undefined).toBeTruthy();
   });
 
   test("Publish Version Test", (): void => {
-    ReactGlobalComponentsLocal('../plugins');
-    // @ts-expect-error: Typescript can not read global variable
-    expect(lodash.VERSION !== undefined).toBeTruthy();
+    ReactGlobalComponents('../../../plugins');
+    expect($this.lodash.VERSION !== undefined).toBeTruthy();
   });
 });
 
 describe('Local Version Test', (): void => {
   test("lodash is undefined?", (): void => {
-    // @ts-expect-error: Typescript can not read global variable
-    window.lodash = undefined
-    // @ts-expect-error: Typescript can not read global variable
-    expect(lodash === undefined).toBeTruthy();
+    $this.lodash = undefined
+    expect($this.lodash === undefined).toBeTruthy();
   });
 
   test("Local Version Test", (): void => {
     ReactGlobalComponentsLocal('../plugins');
-    // @ts-expect-error: Typescript can not read global variable
-    expect(lodash.VERSION !== undefined).toBeTruthy();
+    expect($this.lodash.VERSION !== undefined).toBeTruthy();
   });
 })

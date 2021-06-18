2aa118e34782ff1e31a0a8da6b961f7c267d6cc6
Seokhyeon Jang <coolman555@me.com>
2021-06-18 09:03:04 +0900

    fix: pollyfill and find file name

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,48 @@
+// Pollyfill
+if (typeof require.context === 'undefined') {
+  const fs = require('fs');
+  const path = require('path');
+  require.context = (base: String = '.', scanSubDirectories: Boolean = false, regularExpression: RegExp = /\.js$/): any => {
+    const files: Object = {};
+
+    function readDirectory(directory: any) {
+      fs.readdirSync(directory).forEach((file: String) => {
+        const fullPath: any = path.resolve(directory, file);
+
+        if (fs.statSync(fullPath).isDirectory()) {
+          if (scanSubDirectories) readDirectory(fullPath);
+
+          return;
+        }
+
+        if (!regularExpression.test(fullPath)) return;
+
+        files[fullPath] = true;
+      });
+    }
+
+    readDirectory(path.resolve(__dirname, base));
+
+    function Module(file: any): NodeRequire {
+
+      return require(file);
+    }
+
+    Module.keys = (): String[] => Object.keys(files);
+
+    return Module;
+  };
+}
+
 // Import Global
 function importGlobal(requireContext: __WebpackModuleApi.RequireContext): void {
   requireContext.keys().map((key: string) => {
-    window[key.replace(".js", "").replace("./", "")] = requireContext(
+    window[key.replace(".js", "").replace(/^.*[\\\/]/, '')] = requireContext(
       key
     ).default;
   });
 }
 
 export default function ReactGlobalComponents(path: String = "/plugins"): void {
-  importGlobal(require.context(`../../..${path}`, true, /\.js$/));
+  importGlobal(require.context(`${require('path').resolve(__dirname)}/${path}`, true, /\.js$/));
 }
--- tests/publish.test.ts
+++ tests/publish.test.ts
@@ -1,8 +1,20 @@
+/*
 // @ts-expect-error: Not ready support typescript
 import ReactGlobalComponents from "react-global-components";
 
 test("Publish Version Test", () => {
-  ReactGlobalComponents("/plugins");
+
+  console.log(ReactGlobalComponents)
+  ReactGlobalComponents();
+  // @ts-expect-error: Typescript can not read global variable
+  expect(lodash.VERSION !== undefined).toBeTruthy();
+});
+*/
+
+import ReactGlobalComponents from "../src/index";
+
+test("Publish Version Test", () => {
+  ReactGlobalComponents('../plugins');
   // @ts-expect-error: Typescript can not read global variable
   expect(lodash.VERSION !== undefined).toBeTruthy();
 });

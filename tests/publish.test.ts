23545fd72b5a686c098389246110c81dbdfe3049
Seokhyeon Jang <coolman555@me.com>
2021-06-18 09:15:25 +0900

    fix: test case fix error

--- tests/publish.test.ts
+++ tests/publish.test.ts
@@ -4,24 +4,30 @@ import ReactGlobalComponentsLocal from "../src/index";
 
 describe('Publish Version Test', (): void => {
   test("lodash is undefined?", (): void => {
+    // @ts-expect-error: Typescript can not read global variable
     window.lodash = undefined
+    // @ts-expect-error: Typescript can not read global variable
     expect(lodash === undefined).toBeTruthy();
   });
 
   test("Publish Version Test", (): void => {
     ReactGlobalComponentsLocal('../plugins');
+    // @ts-expect-error: Typescript can not read global variable
     expect(lodash.VERSION !== undefined).toBeTruthy();
   });
 });
 
 describe('Local Version Test', (): void => {
   test("lodash is undefined?", (): void => {
+    // @ts-expect-error: Typescript can not read global variable
     window.lodash = undefined
+    // @ts-expect-error: Typescript can not read global variable
     expect(lodash === undefined).toBeTruthy();
   });
 
   test("Local Version Test", (): void => {
     ReactGlobalComponentsLocal('../plugins');
+    // @ts-expect-error: Typescript can not read global variable
     expect(lodash.VERSION !== undefined).toBeTruthy();
   });
 })
